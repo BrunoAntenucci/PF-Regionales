e14bfa418f4041eb940953a773c33eb2bb89b085
BrunoAntenucci <brunoesantenucci@gmail.com>
2021-08-30 18:53:15 -0300

    Init

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
+
+
+server.use(express.urlencoded({ extended: true, limit: "50mb"}));
+server.use(express.json({limit: "50mb"}))
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+
+server.listen(3001, () => {
+    console.log('Listening at 3001')
+})
