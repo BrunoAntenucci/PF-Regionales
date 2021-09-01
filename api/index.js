1b03914f861b786b04a4246718eebbd2229d068f
Mati Alvarez <matialvarez.arg@gmail.com>
2021-09-01 15:46:28 -0300

    No le den bola, es un backup de mi index.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const server = express();
+const server = require("./src/app");
 const mongoose = require("mongoose");
 
+const dbURL = "mongodb+srv://matitest:matias123@cluster0.y5xbo.mongodb.net/E-Commerce?retryWrites=true&w=majority"
 
-server.use(express.urlencoded({ extended: true, limit: "50mb"}));
-server.use(express.json({limit: "50mb"}))
-server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+const port = process.env.PORT || 8080;
 
-
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-
-server.listen(3001, () => {
-    console.log('Listening at 3001')
-})
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => server.listen(port, () => {
+        console.log(`Server listen at port: ${port} & DB connected Mongo Atlas`)
+    }))
+    .catch((err) => {
+      return next(err)
+    })
