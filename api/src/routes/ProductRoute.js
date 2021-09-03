c5b9e3493a00f9d46bd68f9d9b5396c2b47ad014
Hernán <41600478+hastigarraga@users.noreply.github.com>
2021-09-02 21:36:17 -0300

    crud de products

--- api/src/routes/ProductRoute.js
+++ api/src/routes/ProductRoute.js
@@ -3,16 +3,16 @@ const router = Router();
 const Product = require('../models/Product');
 const Category = require('../models/Category');
 
-router.post("/", async (req, res) => {
-    const {name,description,price, quantity, category, image} = req.body; 
+router.post("/product", async (req, res) => {
+    const {user,name,description,price, quantity, category, image} = req.body; 
     console.log(req.body)
-    if (!name || !description || !price  || !category || !quantity || !image) {
+    if (!user, !name || !description || !price  || !category || !quantity || !image) {
         return res.status(400).json({
             error: 'Something is missing!',
         });
     } 
 
-    const newProduct = await new Product({name, description, price, category , quantity, image})
+    const newProduct = await new Product({user, name, description, price, category , quantity, image})
     const product = await newProduct.save();
     res.json(product);
 
@@ -25,7 +25,7 @@ router.post("/", async (req, res) => {
     // }
 });
 
-router.get("/", (req, res) => {
+router.get("/product", (req, res) => {
     Product.find({}, (err, products) => {
       Category.populate(products, { path: "category" }, (err, products) => {
         res.status(200).send(products);
@@ -33,28 +33,59 @@ router.get("/", (req, res) => {
     });
   });
 
-router.get("/:id", async (req, res) => { 
+
+router.get("/product/search/:name", async (req, res) => {
+    const { name } = req.params;
+    const nameProduct = await Product.find({name:{ $regex: name, $options:'i' }});
+    return res.status(200).send(nameProduct);
+});
+
+
+router.get("/product/:id",  (req, res) => { 
     const {id} = req.params; 
-    Product.find({ _id: id }, function(err, product) {
+    
+      Product.find({ _id: id }, (err, product) => {
         if (err) {
             return res.json({msj: 'Product missing!'});
         } else {
             return res.json({product});
         }
     })
+    
+
 });
 
+router.put("/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Product.findByIdAndUpdate({ _id: id },{ ...req.body });
+        res.send("Product updated!");
+    } catch (err) {
+        console.log("Error: " + err);
+    }
+
+});
+router.delete("/product/:id", async (req, res) => {
+    try {
+      const {id }= req.params;
+      await Product.deleteOne({ _id: id });
+      res.send("Product deleted!");
+    } catch (err) {
+      console.log("Error", err);
+    }
+  });
+
 /*
-"_id": "61302cf18fe3fe25b9bb6ea2",
+"_id": "61316496a16c17b5b015bc6f",
     "name": "lacteos",
 
-    "_id": "61302d138fe3fe25b9bb6ea5",
+    "_id": "6131648ca16c17b5b015bc6c",
     "name": "electronica",
 
     "_id": "6130f92ff3d685f0abf597bf",
     "name": "bebidas",
 
-    "_id": "61302d648fe3fe25b9bb6eab",
+    "_id": "6131649ca16c17b5b015bc72",
     "name": "carnes",
 */
 
