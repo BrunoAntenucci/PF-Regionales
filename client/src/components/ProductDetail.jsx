69ce2d138d903bc35f370445d544b946d41a95b4
Franco <francomartin.personal@gmail.com>
2021-09-04 14:23:09 -0300

    detail with details

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -8,16 +8,53 @@ import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        boxShadow:" 10px 5px 5px #0002",
+      borderRadius:"10px",
+      width: "fit-content",
+      margin:"220px auto",
+      padding:"20px",
+      background:"#eee",
     },
     paper: {
-      padding: theme.spacing(2),
+      padding: theme.spacing(1),
+      margin:"0",
       textAlign: 'center',
       color: theme.palette.text.secondary,
+      textAlign:"justify"
     },
     image: {
-        width:"200px",
-        height:"200px",
+        width:"500px",
+        height:"500px",
+        borderRadius:"10px",
+        border:"1px solid"+theme.palette.secondary.light  
+    },
+    imageDiv:{
+        padding:" 10px 40px",
+    },
+    content:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"center"
+
+
+    },
+    info:{
+        background:"#fff",
+        padding:"10px",
+        borderRadius:"10px",
+        textAlign:"justify",
+        margin:"20px 0",
+        border:"1px solid"+theme.palette.primary.main
+    },
+    cname:{
+        fontSize:"22px",
+        color:"#333",
+        fontFamily:"tahoma"
+    },
+    cprice:{
+        fontSize:"36px",
+        color:"black",
+        fontFamily:"roboto"
     }
   }));
 
@@ -42,25 +79,35 @@ function ProductDetail(props) {
             {
                 detail.product?.map(p => {
                     return (
-                        <div>    
-                            <Grid container spacing={4} direction="column"
-                            justifyContent="center"
-                            alignItems="center">
+                        <div className={classes.content}>
+                        <div className={classes.contentLeft}>       
+                    <div className={classes.imageDiv}>                       
+                <img src={p?.image} className={classes.image} alt="prod_img"/>
+               
+                        </div>
+                         </div>     
+                         <div className={classes.contentRight}>     
+                             <div className={classes.info}>  
+                            <Grid container spacing={2} direction="column"
+                            justifyContent="flex-start"
+                            alignItems="flex-start">
                                 <Grid item xs>
-                                    <Paper className={classes.paper}><img src={p?.image} className={classes.image} alt="prod_img"/></Paper>
+                                    <h3 className={classes.paper +" "+classes.cname }>{p.name}</h3>
+                                    <h3 className={classes.paper  +" "+classes.cprice} >${p.price}</h3>
+                                    
                                 </Grid>
                             </Grid>
-                            <Grid container spacing={4} direction="column"
-                            justifyContent="center"
-                            alignItems="center">
-                                <Grid item xs>
-                                    <Paper className={classes.paper}>{p.name}</Paper>
-                                    <Paper className={classes.paper}>{p.description}</Paper>
-                                    <Paper className={classes.paper}>Categoria: {p.category}</Paper>
-                                    <Paper className={classes.paper}>Precio: ${p.price}</Paper>
-                                </Grid>
-                            </Grid>
+                            </div>
+                            <div className={classes.info}>
+                            <h3 className={classes.paper}>Usuario: usuario</h3>
+                                    <h3 className={classes.paper}>puntuación: 5</h3>
                             
+                            </div>
+                            <div className={classes.info}>
+                            <h3 className={classes.paper}>Categoria: {p.category}</h3>
+                                    <h3 className={classes.paper}>{p.description}</h3>
+                            </div>
+                            </div>
                         </div>
                     )
                 })
