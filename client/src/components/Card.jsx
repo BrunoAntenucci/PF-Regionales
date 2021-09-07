2b6bc2ea7933738f6b4a80321cf3dfc04793b08a
Franco <francomartin.personal@gmail.com>
2021-09-07 13:54:58 -0300

    linking in card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -7,27 +7,35 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import cartEmpty from '../img/cart-empty.png'
+import { Link } from 'react-router-dom';
 const useStyles = makeStyles({
   root: {
     minWidth: 300,
     margin: "20px",
-    height: 300,
+    height: "fit-content",
     width: 300,
   },
   media: {
+    margin:"auto", 
     height: 200,
     width: 200,
   },
+  cart:{
+    width:"20px",
+    height:"20px",
+    justifySelf: "end"
+  }
 });
-function Card({name, category, price, image}) {
+function Card({name, category, price, image, id}) {
     const classes = useStyles();
+    console.log(id)
     return (
         <CardMUI 
-        style={{
-          display: 'grid',
-          justifyContent: 'center'}} 
+         
           className={classes.root}>
+            <Link to={'/detail/' + id}
+           style={{textDecoration:"none"}}>
         <CardActionArea>
           <CardMedia
             className={classes.media}
@@ -47,9 +55,10 @@ function Card({name, category, price, image}) {
             </Typography>
           </CardContent>
         </CardActionArea>
+        </Link>
         <CardActions>
           
-          
+          <img src={cartEmpty} className={classes.cart}></img>
         </CardActions>
       </CardMUI>
         
