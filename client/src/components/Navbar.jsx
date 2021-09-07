47086b91e9f178662cf06bf395d2911a76d88be2
flamarusoto <flaviamsoto@outlook.com>
2021-09-07 14:14:08 -0300

    cookie

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import NavBarMati from './NavBarMati';
+
 import { logOutMati } from "../actions/index"
 import User from './User';
 
