4bb4d9540c533ee79afd57762862be4067a433e8
BrunoAntenucci <brunoesantenucci@gmail.com>
2021-09-09 23:30:43 -0300

    Antes de deploy

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT 
 
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import iconUser from '../img/icon-user.png'
-import axios from 'axios';
+//import axios from 'axios';
 //------IMPORT ACTIONS------//
 import { checkUser, logOut } from '../actions/index';
 
