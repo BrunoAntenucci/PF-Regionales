8f2eea8483f583f325a6ba8835a80b0fba0d9f13
Sofia <sraissiguier@gmail.com>
2021-09-11 12:36:57 -0300

    Fix bug

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
-import iconUser from '../img/icon-user.png'
+import iconUser from '../img/icon-user.png';
+import Cart from './Cart';
 
 import { getCartByUser } from '../actions/index';
 import axios from 'axios';
