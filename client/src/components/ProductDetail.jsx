f3d3dea74a5512c944dbd8ad4525c2411e5d1550
Franco <francomartin.personal@gmail.com>
2021-09-11 17:51:59 -0300

    Icon modify redirection Done!

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -9,6 +9,10 @@ import Grid from '@material-ui/core/Grid';
 import market from '../img/market.png'
 import { Button, Typography } from '@material-ui/core';
 import cartEmpty from '../img/cart-empty.png'
+import iconChange from '../img/change-icon.png'
+import BuildOutlinedIcon from '@material-ui/icons/BuildOutlined';
+
+
 //--------IMPORT ACTIONS-----------//
 import { addProductToCart, removeProductFromCart } from '../actions/index';
 const useStyles = makeStyles((theme) => ({
@@ -223,6 +227,7 @@ function ProductDetail(props) {
             console.log("No existe ese producto en el LocalStorage")
         }
     }
+    console.log(detail.product)
     //---------------LOCAL STORAGE--------------------
     // useEffect(() => {
     //     const localStorageContent = localStorage.getItem('history');
@@ -265,6 +270,21 @@ function ProductDetail(props) {
                    
                          </div>     
                          <div className={classes.contentRight}>     
+                         <div className={classes.cardDiv}
+                                        >
+                                <Typography
+                                    className={classes.cardTypo}
+                                    variant="body1" color="primary" component="p"                                  
+                                    >
+                                        <Link to={"/modifyProduct/"+p._id}
+                                        style={{textDecoration:"none",color:"inherit"}}>
+                                   modificar producto
+                                   </Link>
+                                        </Typography>
+                                        {/*le puse la llave mía porque no me mostraba la del icon*/}
+                                    <img src={iconChange?iconChange:BuildOutlinedIcon}
+                                     className={classes.cart}></img>
+                                    </div>
                              <div className={classes.info}>  
                             <Grid container spacing={2} direction="column"
                             justifyContent="flex-start"
