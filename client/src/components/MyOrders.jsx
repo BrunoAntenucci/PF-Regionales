f6eebbfc22e83a414adb157811c70b278300e115
Franco <francomartin.personal@gmail.com>
2021-09-14 21:01:29 -0300

    orders style done

--- client/src/components/MyOrders.jsx
+++ client/src/components/MyOrders.jsx
@@ -1,121 +1,163 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getOrderDetail } from '../actions';
 import { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core';
-import { Typography } from '@material-ui/core/styles/createTypography';
 
-const useStyles = makeStyles(()=>({
-    root:{
-        width:"70%",
-        margin:"0 auto"
-    },productDes:{
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-around"
-    },orders:{
-        maxWidth:"600px"
-    }
-    ,divItems:{
-        display:"flex",
-        flexDirection:"row",
-        justifyContent:"space-around"
-    },itemsP:{
-        width:"150px",
-    },pName:{
+
+const useStyles = makeStyles((e) => ({
+    root: {
+        width: "95%",
+        margin: "0 auto"
+    }, productDes: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around"
+    },list:{
+        color:e.palette.primary.dark,
+        background: "linear-gradient(60deg, #ffffff 0%, "+e.palette.primary.light+" 75%, rgba(255,253,253,1) 75%,  rgba(255,253,253,1) 76%, "+e.palette.primary.light+" 76%, "+e.palette.primary.light+ " 78%, rgba(255,253,253,1) 78%)",
+    }, orders: {
         
-       
-    },total:{
-        fontWeight:"800"
-    },
-    divExtra:{
-       textAlign:"center"
+        display: "flex",
+        margin: "20px",
+        flexWrap:"wrap"
+    }, orderN: {
+        maxWidth: "600px",
+        margin: "0px 20px 100px",
+        padding:"20px", 
+        background: "#eee",
+    }, products: {
+        overflow: "auto",
+        height: "200px"
     }
-    ,extra:{
-        display:"flex",
-        flexDirection:"column",
-        justifyContent:"center" 
-    },numOfOrder:{
-        maxWidth:"600px",
-        height:"fit-content"
+    , divItems: {
+
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around"
+    }, itemsP: {
+        width: "150px",
+    }, pName: {
+        color: e.palette.secondary.main
+
+    }, total: {
+        fontWeight: "800",
+        fontSize:"1.4em"
+    },
+    divExtra: {
+        textAlign: "center"
+    }
+    , extra: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
+    }, numOfOrder: {
+        maxWidth: "600px",
+        height: "fit-content"
     }
 }))
 const MyOrders = () => {
     const user = useSelector(state => state.user);
     const orderDetail = useSelector(state => state.orderDetail);
     const dispatch = useDispatch();
-   const classes = useStyles()
+    const classes = useStyles()
     useEffect(() => {
         dispatch(getOrderDetail());
-    },[])
-    
-    return (
-        <div className={classes.root}>
-            <h1>Mis compras</h1>
-            <Button
-            style={{height:"min-content"}}
-                 variant="contained" color="primary">
-                   <Link to='/products' style={{textDecoration:"none", color:"white"}}>Volver</Link>
-                    </Button>
-            <h3>Listado de Ordenes</h3>
-            <div className={classes.orders}>
-           {
-               orderDetail.map(order => {
-                   return(
-                       <>
-                       <hr></hr>
-                       <div className={classes.productDes}> 
-                       <p className={classes.itemsP}>Productos: </p>
-                       <p className={classes.itemsP}>cantidad: </p>
-                       <p className={classes.itemsP}>subtotal: </p>
-                    
-                       </div>
-                       <hr></hr>
-                            {order.cart?.items.map(item => {return(
-                       
-                           
-                           <div className={classes.divItems}>
-                               <p className={classes.itemsP +" "+classes.pName}>
-                            <Link to={`/detail/${item.product._id}`}>{item?.product?.name}</Link>
-                            </p >
-                            <p className={classes.itemsP}>
-                                {item.quantity}</p>
-                                <p className={classes.itemsP}>
-                                {item.subTotal}
-                                </p>
-                           </div>
-                       )} )}
-                      <hr/>
-                      <div className={classes.divItems}>
-                      <p className={classes.total} >total</p>
-                        <p className={classes.total} >{order.cart.total}</p>
-                        </div>
-                      <hr/>
-                      <div className={classes.divExtra}>
+    }, [])
 
-                      <div className={classes.extra}>
-                      <p>
-                           número de Orden</p>
-                            <p >
-                           { order._id}
-                           </p>
-                           </div>
-                           <div className={classes.extra}>
-                           <p>Fecha y hora de la compra:</p>
-                       <p> {order.createdAt}</p>
-                       </div>
-                       </div>
-                       <hr></hr>
-                        </>
-                    )
-                }
-               )
-            }
+    return (
+        <>
+            <Button
+                style={{ height: "min-content" }}
+                variant="contained" color="primary">
+                <Link to='/products' style={{ textDecoration: "none", color: "white" }}>Volver</Link>
+            </Button>
+            <div className={classes.root}>
+                <Typography variant="h2"
+                 className={classes.list}
+                 >Mis compras</Typography>
+
+                <Typography variant="h6"
+                
+                >Listado de Ordenes</Typography>
+                <div className={classes.orders}>
+                    {
+                        orderDetail.map(order => {
+                            return (
+                                <>
+                                    <div className={classes.orderN}>
+                                        <hr></hr>
+                                        <div className={classes.productDes}>
+                                            <p className={classes.itemsP}>Productos </p>
+                                            <p className={classes.itemsP}>cantidad </p>
+                                            <p className={classes.itemsP}>subtotal </p>
+
+                                        </div>
+                                        <hr></hr>
+                                        <div className={classes.products}>
+                                            {order.cart?.items.map(item => {
+                                                return (
+
+
+                                                    <div className={classes.divItems}>
+                                                        <p
+                                                            className={classes.itemsP + " " + classes.pName}>
+                                                            <Link to={`/detail/${item.product._id}`}
+                                                                style={{ textDecoration: "none", color: "inherit" }}
+                                                            >{item?.product?.name}</Link>
+                                                        </p >
+                                                        <p className={classes.itemsP}>
+                                                            {item.quantity}</p>
+                                                        <p className={classes.itemsP}>
+                                                            {item.subTotal}
+                                                        </p>
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                        <hr />
+                                        <div className={classes.divItems}>
+                                            <p className={classes.total} >total</p>
+                                            <p className={classes.total} >{order.cart.total}</p>
+                                        </div>
+                                        <hr />
+                                        <div className={classes.divExtra}>
+
+                                           
+                                                <p style={{fontWeight:"600"}}
+                                                >
+                                                    número de Orden</p>
+                                                <p >
+                                                    {order._id}
+                                                </p>
+                                           
+                                                <p style={{fontWeight:"600"}}
+                                                >status:</p>
+                                            {order.status=="Procesando"?
+                                             <p style={{color:"#aa4"}}>
+                                                  {order.status}</p>:
+                                             <p> {order.status}</p> }
+
+                                                <p style={{fontWeight:"600"}}
+                                                >Fecha y hora de la compra:</p>
+                                                <p> {order.createdAt}</p>
+                                        
+                                          
+                                         
+                                        </div>
+                                        <hr></hr>
+                                    </div>
+                                </>
+                            )
+                        }
+                        )
+                    }
+                </div>
             </div>
-        </div>
-               )
+        </>
+    )
 }
 
 
