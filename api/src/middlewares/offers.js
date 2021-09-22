d564cce3a73574c5f050197b5b5673063c4fc67e
Sofia <sraissiguier@gmail.com>
2021-09-22 12:16:39 -0300

    Apply offer middleware

--- api/src/middlewares/offers.js
+++ api/src/middlewares/offers.js
@@ -5,11 +5,6 @@ module.exports = async function offers(req, res, next) {
     const daysArray = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
     const date = new Date();
     const dayName = daysArray[date.getDay()]
-    const productsInOfferLUNES = [];
-    const productsInOfferMIERCOLES = [];
-    const productsInOfferVIERNES = [];
-
-
 
     const allProducts = await Product.find({}).populate("category")
     const allCategory = await Category.find({})
