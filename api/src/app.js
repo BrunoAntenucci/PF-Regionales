f8a023aeb6b9aa1a975502097a07394a3cf37c13
Sofía Raissiguier <78318499+sophiiaR@users.noreply.github.com>
2021-09-23 19:26:49 -0300

    Change cors origin

--- api/src/app.js
+++ api/src/app.js
@@ -24,7 +24,7 @@ server.use(express.urlencoded({ extended: true}));
 server.use(express.json());
 server.use(cors({
 
-  origin: "http://localhost:3000", // DEPLOY: https://pf-regionales.vercel.app // DEV: http://localhost:3000
+  origin: "https://pf-regionales.vercel.app", // DEPLOY: https://pf-regionales.vercel.app // DEV: http://localhost:3000
   credentials: true
 }));
 server.use(session({
