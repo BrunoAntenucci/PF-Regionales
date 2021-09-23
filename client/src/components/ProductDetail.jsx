c4a6b579d59e739cc7ab1fa7fa65aa7209289c0a
Mati Alvarez <matialvarez.arg@gmail.com>
2021-09-23 02:31:33 -0300

    Carrito arreglado.

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
 import { useEffect } from 'react';
@@ -122,73 +122,105 @@ const useStyles = makeStyles((theme) => ({
 
 function ProductDetail(props) {
 
+    const dispatch = useDispatch();
     const classes = useStyles();
-    const history = useHistory();
     const detail = useSelector((state) => state.prodDetail);
     const user = useSelector((state) => state.user);
+    const categories = useSelector((state) => state.categories)
 
     console.log(props.match.params.id, 'id')
-    console.log(detail, "detallewqsdqwd")
-    
-    const dispatch = useDispatch();
+    console.log(detail, "detalle")
 
-    useEffect(() => {
-        dispatch(getCategories());
-        
-    }, [dispatch])
-    useEffect(() => {
-        var product =   detail?.product?.find( e => e)
-        document.title = product?.name
-        return(()=>{
-            document.title = "E-Market"
-            
-        })
-    }, [detail?.product])
-
-    const categories = useSelector((state) => state.categories)
-    
     useEffect(() => {
         dispatch(getProductDetail(props.match.params.id));
-    },[dispatch, props.match.params.id]);
+        dispatch(getCategories());
+        return ()=>{
+            document.title = "E-Market"
+        }
+    }, [])
 
-    const handleCartClick = (detail) => {
-        let historial = [];
+    const handleCartClick = async (detail) => {
+        let historial = { 
+            items: [],
+            total: 0
+        };
+        const item = {
+            product: {
+                _id: detail[0]._id,
+                price: parseInt(detail[0].price),
+                name: detail[0].name,
+                description: detail[0].description,
+                image: detail[0].image,
+            },
+            quantity: 1,
+            subTotal: parseInt(detail[0].price)
+        }
 
-        if(!localStorage.getItem('history')) {
-            historial.push(detail[0]);
-            localStorage.setItem('history', JSON.stringify(historial));
-        } else {
+        if (user) {
+            dispatch(addProductToCart(item.product._id, parseInt(item.product.price)))
+        }
+        if(!localStorage.history && !user) {
+            historial.items.push(item)
+            historial.total += item.product.price;
+            return localStorage.setItem('history', JSON.stringify(historial));
+        } 
+        if (localStorage.history && !user) {
             historial = JSON.parse(localStorage.getItem('history'));
-
-            if(!historial.some(p=> detail.map(pd => pd._id).includes(p._id)) ) {
-                historial.push(...detail);
+            console.log("HISTORIAL: ", historial)
+            for (var i=0; i<historial.items.length; i++) {
+                if (historial.items[i].product._id === item.product._id) {
+                    historial.items[i].quantity++;
+                    historial.items[i].subTotal += item.product.price;
+                    historial.total += item.product.price;
+                    return localStorage.setItem('history', JSON.stringify(historial));
+                } 
             }
-    
+            historial.total += item.product.price;
+            historial.items.push(item)
             localStorage.setItem('history', JSON.stringify(historial));
         }
-        console.log(JSON.parse(localStorage.getItem('history')))
     }
-    
-    //---------------LOCAL STORAGE--------------------
-    // useEffect(() => {
-    //     const localStorageContent = localStorage.getItem('history');
-
-    // let historial;
-    // if(!localStorageContent) {
-    //     historial = [];
-    // } else {
-    //     historial = JSON.parse(localStorageContent);
-    // }
-    // console.log('history', localStorageContent);
-    // console.log('historial', historial);
-
-    // if(!historial.some(p=> detail.product.map(pd => pd._id).includes(p._id)) ) {
-    //     historial.push(...detail.product);
-    // }
-    
-  
-    // localStorage.setItem('history', JSON.stringify(historial));
-    // }, [detail.product])
+    const handleRemoveCart = (detail) => {
+        let historial = { 
+            items: [],
+            total: 0
+        };
+        const item = {
+            product: {
+                _id: detail[0]._id,
+                price: parseInt(detail[0].price),
+                name: detail[0].name,
+                description: detail[0].description,
+                image: detail[0].image,
+            },
+            quantity: 1,
+            subTotal: parseInt(detail[0].price)
+        }
+        if (user) {
+            dispatch(removeProductFromCart(item.product._id, parseInt(item.product.price)))
+        }
+        if(!localStorage.history && !user) {
+            console.log("No hay localStorage ni User. Nada que hacer")
+        } 
+        if (localStorage.history && !user) {
+            console.log("Hay Storage sin User.")
+            historial = JSON.parse(localStorage.getItem('history'));
+            for (var i=0; i<historial.items.length; i++) {
+                if (historial.items[i].product._id === item.product._id) {
+                    historial.items[i].quantity--;
+                    historial.items[i].subTotal -= item.product.price;
+                    historial.total -= item.product.price;
+                    if (historial.items[i].quantity === 0) {
+                        historial.items.splice(i, 1);
+                    }
+                    console.log("Producto eliminado");
+                    i=-1;
+                    return localStorage.setItem('history', JSON.stringify(historial));
+                }
+            }
+            console.log("No existe ese producto en el LocalStorage")
+        }
+    }
     
     //------------------------------------------------
 
