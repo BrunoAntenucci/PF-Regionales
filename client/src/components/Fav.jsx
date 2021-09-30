fb54e4240305f935d29ac5c943c7e0c2e04c2a38
Sofia <sraissiguier@gmail.com>
2021-09-30 16:52:09 -0300

    Add alert to fav button in fav component

--- client/src/components/Fav.jsx
+++ client/src/components/Fav.jsx
@@ -3,8 +3,10 @@ import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addFav, deleteFav, getFav } from '../actions';
+import Notification from './Notification';
 
 export default function Fav ({id}) {
+    const [notify, setNotify] = useState({ isOpen: false, message: '', type: '' })
     const dispatch = useDispatch();
     const user = useSelector(state => state.user)
     const wishlist = useSelector(state => state.wishlist);
@@ -18,6 +20,11 @@ export default function Fav ({id}) {
         if(user) {
             dispatch(addFav(id))
             setFav(true)
+            setNotify({
+                isOpen: true,
+                message: 'Producto añadido a Favoritos',
+                type: 'success'
+            })
         }
     }
     const handleDeleteFav = async(e) => {
@@ -36,6 +43,10 @@ export default function Fav ({id}) {
                         <AiFillHeart className='fav' />
                     </button></div>)
                 }  
+            <Notification
+                notify={notify}
+                setNotify={setNotify}
+            />
         </div>
     )
 }
