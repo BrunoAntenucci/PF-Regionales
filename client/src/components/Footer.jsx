14c8a44dbb8b438e4dc9ada33581d803d3cd723a
Franco <francomartin.personal@gmail.com>
2021-09-30 22:26:27 -0300

    footer grey and color leyend main pink

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((e)=>({
   footer:{
   padding: "40px",
-  background: "#32323f",
+  background: "grey",
 //background: "linear-gradient(112deg,"+e.palette.secondary.main+" 11%, "+e.palette.secondary.dark+" 33%, "+e.palette.secondary.main+" 55%)",
 
   color:"white",
